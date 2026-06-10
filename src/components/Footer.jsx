6ddf694f071b468b36a4a27b7e3e8a02382e94fe
Fulan <Fulan@gmail.com>
2026-06-10 08:56:50 +0700

    push 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full text-center py-6 border-t border-zinc-900">
       <p className="text-xs text-zinc-500 tracking-wide">
-        &copy; {new Date().getFullYear()} AN.PRASETYA. All rights reserved.
+        &copy; {new Date().getFullYear()} Tsany.AF All rights reserved.
       </p>
     </footer>
   );
